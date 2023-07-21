6a096ccd225dfc127d33970a45ad3dafbeea1430
helena <heju1958@outlook.com>
2023-07-21 09:28:01 -0300

    feat: update api

--- mobilescreen/src/services/index.ts
+++ mobilescreen/src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api-crud-employees.onrender.com",
+  baseURL: "https://api-crud-python-2023.onrender.com",
   timeout: 5000,
 });
 
